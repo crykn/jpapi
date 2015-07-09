14a7ba58e212e5bd19a8288fb9a4574a67686d7f
crykn <crykn@users.noreply.github.com>
2015-07-09 16:50:54 +0200

    Updated the javadoc and added a private constructor

--- Pewn API/src/main/java/de/damios/jpapi/core/Constants.java
+++ Pewn API/src/main/java/de/damios/jpapi/core/Constants.java
@@ -2,8 +2,14 @@ package de.damios.jpapi.core;
 
 /**
  * Enthält alle Konstanten, die im Wrapper verwendet werden
+ *
+ * @author damios
+ * @since 1.0
  */
 public class Constants {
+	
+	private Constants(){
+	}
 
 	/**
 	 * Enthält die Adresse des Hosts, an den alle Anfragen gehen ({@value})
