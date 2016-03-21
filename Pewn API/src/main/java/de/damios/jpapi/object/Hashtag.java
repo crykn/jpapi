f52d4bb2fc55f4242f156bcb04118001e1891777
crykn <crykn@users.noreply.github.com>
2016-03-21 16:41:07 +0100

    Added support for Hashtags

---
+++ Pewn API/src/main/java/de/damios/jpapi/object/Hashtag.java
@@ -0,0 +1,81 @@
+package de.damios.jpapi.object;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.annotations.SerializedName;
+
+import de.damios.jpapi.core.ApiRequest;
+
+/**
+ * Java Repräsentierung des JSON-Hashtag-Objekts
+ * 
+ * @author damios
+ * @version 0.4.0
+ */
+public class Hashtag implements Serializable {
+
+	private static final long serialVersionUID = 100L;
+	private int id;
+	private String name;
+	private int usedInProjectsCount;
+	@SerializedName("hashtagCategory")
+	private HashtagCategory category;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getUsedInProjectsCount() {
+		return usedInProjectsCount;
+	}
+
+	public HashtagCategory getCategory() {
+		return category;
+	}
+
+	/**
+	 * Liefert ein Array aller Hashtags eines Spiels
+	 * 
+	 * @param gameid
+	 *            Die Spieleid
+	 * @return Hashtag-Array
+	 * @throws IOException
+	 *             wenn ein Fehler beim Ausführen der Anfrage auftritt
+	 * @throws JsonSyntaxException
+	 *             wenn ein Fehler beim Parsen auftritt
+	 * @see ApiRequest#execute(String, Class)
+	 */
+	public static Hashtag[] get(int gameid) throws JsonSyntaxException,
+			IOException {
+		return ApiRequest.execute("v1/game/id/" + gameid + "/hashtags",
+				Hashtag[].class);
+	}
+
+	/**
+	 * Java Repräsentierung des JSON-Hashtag-Kategorie-Objekts
+	 * 
+	 * @author damios
+	 * @version 0.4.0
+	 */
+	public class HashtagCategory implements Serializable {
+
+		private static final long serialVersionUID = 100L;
+		private int id;
+		private String name;
+
+		public int getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+
+}
